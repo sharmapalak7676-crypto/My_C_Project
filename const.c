8aaf6cf61267f3c96966d7247a11b99725e4d901
sharmapalak7676-crypto <sharmapalak7676@gmail.com>
2025-11-21 23:08:47 +0530

    Create const.c

---
+++ const.c
@@ -0,0 +1,14 @@
+// tokens 
+// constant 
+
+=> Constants are fixed values that cannot be changed during program execution.
+They are used to store data that should remain the same throughout the program.
+
+  // wap with using constant
+  #include<stdio.h>
+int main(){
+    const int a = 10;
+    printf("%d", a);
+}
+
+output=> 10
