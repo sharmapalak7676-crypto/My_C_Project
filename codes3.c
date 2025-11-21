af70bbfe52728f647aea84e271c0419487707e8b
sharmapalak7676-crypto <sharmapalak7676@gmail.com>
2025-11-21 23:25:04 +0530

    Create codes3.c

---
+++ codes3.c
@@ -0,0 +1,18 @@
+// wap to find the area of rectangle //
+
+#include <stdio.h>
+int main() {
+    int length, width, area;
+    printf("Enter length: ");
+    scanf("%d", &length);
+    printf("Enter width: ");
+    scanf("%d", &width);
+    area = length * width;
+    printf("Area of rectangle = %d", area);
+    return 0;
+}
+
+//output=>
+Enter length: 5
+Enter width: 4
+Area of rectangle = 20
