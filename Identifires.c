e5141bb165c14b959b4e192829bf4fad43dabdb1
sharmapalak7676-crypto <sharmapalak7676@gmail.com>
2025-11-21 23:04:44 +0530

    Create Identifires.c

---
+++ Identifires.c
@@ -0,0 +1,15 @@
+// tokens
+// identifiers 
+
+Identifiers are names given to variables, functions, arrays, and other user-defined elements in a program.
+
+C code ( for identifiers)
+#include<stdio.h>
+int main(){
+    int age = 20;
+    float marks = 88.5;
+    char grade = 'A';
+    printf("%d %.1f %c", age, marks, grade);
+}
+
+op => 20 88.5 A
