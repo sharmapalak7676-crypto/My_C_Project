ac9d386239a5c867e59da92d8aaa1901b877f07b
sharmapalak7676-crypto <sharmapalak7676@gmail.com>
2025-11-21 23:20:48 +0530

    Create codes.c

---
+++ codes.c
@@ -0,0 +1,23 @@
+// SOME CODES OF USING TOKENS AND DATATYPES
+
+ // WAP TO ADD TWO NUMBERS INTO THIRD VARIABLE BY TAKING INPUT FROM USER //
+
+
+#include <stdio.h>
+int main() {
+    int a, b, c;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    c = a + b;
+    printf("Sum = %d", c);
+    return 0;
+}
+
+ output=>
+   Enter first number: 5
+Enter second number: 7
+Sum = 12
+
+
