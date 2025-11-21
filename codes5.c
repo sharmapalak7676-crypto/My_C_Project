d762222e21fa10de3a60326664a24f8a78253636
sharmapalak7676-crypto <sharmapalak7676@gmail.com>
2025-11-21 23:28:41 +0530

    Create codes5.c

---
+++ codes5.c
@@ -0,0 +1,14 @@
+// WAP TO PRINT THE ASCII VALUE OF CHARACTER //
+
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    printf("ASCII value = %d", ch);
+    return 0;
+}
+
+output=>
+Enter a character: A
+ASCII value = 65
