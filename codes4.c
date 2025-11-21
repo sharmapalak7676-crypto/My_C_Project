0bdea152fa4909e63b36692de93a05086612dd2e
sharmapalak7676-crypto <sharmapalak7676@gmail.com>
2025-11-21 23:26:40 +0530

    Create codes4.c

---
+++ codes4.c
@@ -0,0 +1,15 @@
+// wap to find the circumfrence of circle //
+
+#include <stdio.h>
+int main() {
+    float r, c;
+    printf("Enter radius: ");
+    scanf("%f", &r);
+    c = 2 * 3.14 * r;
+    printf("Circumference of circle = %.2f", c);
+    return 0;
+}
+
+ output=>
+Enter radius: 7
+Circumference of circle = 43.96
