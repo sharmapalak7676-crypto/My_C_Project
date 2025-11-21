86064edb79b338e7e03c6f940bdbc4617a33c406
sharmapalak7676-crypto <sharmapalak7676@gmail.com>
2025-11-21 23:46:12 +0530

    Create codes8.c

---
+++ codes8.c
@@ -0,0 +1,20 @@
+// WAP TO CHECK CHAR IS VOWEL OR CONSONANT //
+
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || 
+       ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') {
+        printf("Vowel");
+    } else {
+        printf("Consonant");
+    }
+    return 0;
+}
+
+//OUTPUT =>
+Enter a character: A
+Vowel
