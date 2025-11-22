2a9f18a8c25dcc2e735b35d13ce94b6a0d82cadb
sharmapalak7676-crypto <sharmapalak7676@gmail.com>
2025-11-22 11:40:05 +0530

    Create whilecodes12.c

---
+++ whilecodes12.c
@@ -0,0 +1,13 @@
+// wap to print first 10 numbers using while loop //
+
+#include <stdio.h>
+int main() {
+    int i = 1;
+    while(i <= 10) {
+        printf("%d ", i);
+        i++;
+    }
+    return 0;
+}
+
+
