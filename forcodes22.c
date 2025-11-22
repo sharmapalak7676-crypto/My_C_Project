bf9be02a26979c09734d84066b5e6bcfe13208b6
sharmapalak7676-crypto <sharmapalak7676@gmail.com>
2025-11-22 12:13:07 +0530

    Create forcodes22.c

---
+++ forcodes22.c
@@ -0,0 +1,16 @@
+// wap to print star in a rows in reverse order //
+
+# include <stdio.h>
+int main (){
+    int rows;
+    printf("enter the rows:");
+    scanf("%d",&rows);
+    for( int i=rows; i>=1;i--){
+    for(int j=1; j<=i ;j++){
+
+    printf("*");
+    }
+printf("\n");
+}
+return 0;
+}
