871bb391caebef209ac692a7a9cfc6fa25b84d1d
sharmapalak7676-crypto <sharmapalak7676@gmail.com>
2025-11-22 14:53:58 +0530

    Create codes32.c

---
+++ codes32.c
@@ -0,0 +1,24 @@
+//functions
+//wap to swap two numbers using call by refrence.
+
+//CALL BY REFRENCE=>
+
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swapping: x = %d, y = %d", x, y);
+
+    return 0;
+}
