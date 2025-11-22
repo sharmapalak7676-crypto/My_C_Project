7fff69c1e91231e497b795995dc5cb5cb6761dbd
sharmapalak7676-crypto <sharmapalak7676@gmail.com>
2025-11-22 12:22:37 +0530

    Create codes28.c

---
+++ codes28.c
@@ -0,0 +1,16 @@
+//wap to print all the factors of given number
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factors of %d are: ", num);
+    for(int i = 1; i <= num; i++) {
+        if(num % i == 0)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
+
