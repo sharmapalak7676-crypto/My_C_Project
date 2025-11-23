38bb0cb927d4f3fe1442a02ac49a0bc6e4fa7e57
sharmapalak7676-crypto <sharmapalak7676@gmail.com>
2025-11-23 22:05:11 +0530

    Update codes36.c

--- codes36.c
+++ codes36.c
@@ -1,20 +1,13 @@
 // wap to pirnt even or add numbers from an array
-include <stdio.h>
-
+#include <stdio.h>
 int main() {
-    int arr[100], size, i;
-
-    // Input the size of the array
+    int arr[100], size,i;
     printf("Enter the number of elements: ");
     scanf("%d", &size);
-
-    // Input the elements of the array
     printf("Enter %d elements:\n", size);
     for (i = 0; i < size; i++) {
         scanf("%d", &arr[i]);
     }
-
-    // Check and print whether each element is even or odd
     for (i = 0; i < size; i++) {
         if (arr[i] % 2 == 0)
             printf("%d is even\n", arr[i]);
